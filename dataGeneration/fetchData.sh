77a6c71326642bed128112af42e7d688531c0996
curran <curran.kelleher@gmail.com>
2017-08-09 14:15:09 +0530

    Use suggested data source

--- dataGeneration/fetchData.sh
+++ dataGeneration/fetchData.sh
@@ -1,2 +1,1 @@
-wget http://popstats.unhcr.org/en/time_series.csv
-wget http://data.unhcr.org/api/countries/list.json
+wget http://popdata.unhcr.org/api/stats/time_series_all_years.json?population_type_code=REF -O time_series.json
--- dataGeneration/generateData.js
+++ dataGeneration/generateData.js
@@ -7,74 +7,20 @@
 // But first, run this script that fetches the source data:
 // sh fetchData.sh
 var fs = require('fs'),
-    d3 = Object.assign(
-      require('d3-dsv'),
-      require('d3-collection'),
-      require('d3-array')
-    ),
+    d3 = Object.assign(require('d3-collection'),require('d3-array')),
     outputFile = '../data.js',
-    csvStringRaw = fs.readFileSync('time_series.csv', 'utf8'),
-    csvString = csvStringRaw.substr(170), // Chop off the junk in first few lines.
-    csvData = d3.csvParse(csvString),
-    list = JSON.parse(fs.readFileSync('list.json', 'utf8'));
+    data = JSON.parse(fs.readFileSync('time_series.json', 'utf8'));
 
-// Lookup table mapping country/region names to codes.
-var nameToCodeMap = {};
-list.forEach(function (d) {
-  nameToCodeMap[d.name_en] = d.country_code;
-});
-
-var nameCorrections = {};
-nameCorrections['United Kingdom'] = 'United Kingdom of Great Britain and Northern Ireland';
-nameCorrections['China, Hong Kong SAR'] = 'Hong Kong SAR, China';
-nameCorrections['China, Macao SAR'] = 'Macao SAR, China';
-nameCorrections['Serbia and Kosovo (S/RES/1244 (1999))'] = 'Serbia (and Kosovo: S/RES/1244 (1999))';
-nameCorrections['Cabo Verde'] = 'Cape Verde';
-nameCorrections['Palestinian'] = 'State of Palestine'; // Possibly could be 'Palestinian Territory, Occupied'
-nameCorrections['Holy See (the)'] = 'Holy See';
-nameCorrections['Bonaire'] = 'Bonaire, Sint Eustatius and Saba';
-nameCorrections['Curaçao'] = 'Curacao';
-nameCorrections['Saint-Pierre-et-Miquelon'] = 'Saint Pierre and Miquelon';
-nameCorrections['Wallis and Futuna Islands '] = 'Wallis and Futuna Islands';
-var unknownNames = {};
-
-function correctName(name){
-  name = name.replace('Rep.', 'Republic');
-  name = name.replace('Dem.', 'Democratic');
-  return nameCorrections[name] || name;
-}
-
-function nameToCode(name) {
-  var code = nameToCodeMap[correctName(name)];
-  if(!code){
-    unknownNames[name] = true;
-  }
-  return code;
-}
-
-var augmentedData = csvData
-  .map(function (d) {
-    
-    // Parse the value into a number.
-    d.Value = +d.Value;
-
-    // Convert the origin and asylum names into codes.
-    var originName = d.Origin;
-    var asylumName = d['Country / territory of asylum/residence'];
-    d.ORI = nameToCode(originName);
-    d.ASY = nameToCode(asylumName);
-    d.hasCodes = d.ORI && d.ASY;
-
-    //console.log(d['Population type']);
-    return d;
-  })
-  .filter(function (d) {
-    var refugees = d['Population type'] === 'Refugees (incl. refugee-like situations)';
-    return refugees && d.hasCodes;
-  });
-
-console.log('Unable to find codes for the following places:');
-console.log(Object.keys(unknownNames));
+// The structure we're dealing with for each element of the data array:
+// { year: 1951,
+//   country_of_residence: 'AUS',
+//   country_of_residence_en: 'Australia',
+//   country_of_origin: 'XXC',
+//   country_of_origin_en: 'Various/Unknown',
+//   value: 180000 }
+var residences = data.map(d => d.country_of_residence);
+var origins = data.map(d => d.country_of_origin);
+var allCountryCodes = d3.set(residences.concat(origins)).values();
 
 var yearToDataEntry = {};
 function getDataEntry(year){
@@ -87,9 +33,9 @@ function getDataEntry(year){
     };
 
     // Fill in zero for missing data.
-    list.forEach(function (d) {
-      entry.ORI[0][d.country_code] = 0;
-      entry.ASY[0][d.country_code] = 0;
+    allCountryCodes.forEach(function (country_code) {
+      entry.ORI[0][country_code] = 0;
+      entry.ASY[0][country_code] = 0;
     });
   }
   return entry;
@@ -100,16 +46,16 @@ function asy(year){ return getDataEntry(year).ASY[0]; }
 
 function sumValues(values) {
   return d3.sum(values, function(d) {
-    return d.Value;
+    return d.value;
   })
 }
 
-// Compute the sums for each (Year, ORI) combination
+// Compute the sums for each (year, country_of_origin) combination
 d3.nest()
-  .key(function (d) { return d.Year; })
-  .key(function (d) { return d.ORI; })
+  .key(function (d) { return d.year; })
+  .key(function (d) { return d.country_of_origin; })
   .rollup(sumValues)
-  .entries(augmentedData)
+  .entries(data)
   .forEach(function (d){
     var year = d.key;
     d.values.forEach(function (value) {
@@ -118,12 +64,12 @@ d3.nest()
     });
   });
 
-// Compute the sums for each (Year, ASY) combination
+// Compute the sums for each (year, country_of_residence) combination
 d3.nest()
-  .key(function (d) { return d.Year; })
-  .key(function (d) { return d.ASY; })
+  .key(function (d) { return d.year; })
+  .key(function (d) { return d.country_of_residence; })
   .rollup(sumValues)
-  .entries(augmentedData)
+  .entries(data)
   .forEach(function (d){
     var year = d.key;
     d.values.forEach(function (value) {
